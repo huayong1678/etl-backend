1bc2ae4724c15edf50d2d98849959f815a4a0eba
huayong1678 <poramesjar2000@gmail.com>
2022-05-23 15:36:45 +0700

    Fix Source in Pipeline

--- core/settings.py
+++ core/settings.py
@@ -132,13 +132,17 @@ if 'RDS_DB_NAME' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'localhost',
-            'PORT': 5434,
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #     'NAME': 'postgres',
+        #     'USER': 'postgres',
+        #     'PASSWORD': 'postgres',
+        #     'HOST': 'localhost',
+        #     'PORT': 5434,
+        # }
     }
 
 # Custome User Model
